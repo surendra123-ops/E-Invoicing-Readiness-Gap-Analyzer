239b2b4bc0e9ea3ed57aa0033466fa811cb381ee
surendrakumarreddy79@gmail.com <surendrayenika@gmail.com>
2025-10-06 00:01:53 +0530

    ui changes.,,. 1.,90

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
